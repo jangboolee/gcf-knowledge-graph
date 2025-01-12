47fa68abcc96305287996a1daac6619808fc72da
Jangboo Lee <github@jangboolee.com>
2025-01-13 07:49:17 +0900

    Rename size_dict class

--- src/importer/data_dict/project_size_dict_importer.py
+++
@@ -1,9 +0,0 @@
-from src.importer.base_csv_importer import BaseCsvImporter
-from src.db.db_handler import DBHandler
-from src.db.db_schema import ProjectSizeDict
-
-
-class ProjectSizeDictImporter(BaseCsvImporter):
-
-    def __init__(self, db_handler: DBHandler) -> None:
-        super().__init__(db_handler=db_handler, table_class=ProjectSizeDict)
---
+++ src/importer/data_dict/size_dict_importer.py
@@ -0,0 +1,9 @@
+from src.importer.base_csv_importer import BaseCsvImporter
+from src.db.db_handler import DBHandler
+from src.db.db_schema import SizeDict
+
+
+class SizeDictImporter(BaseCsvImporter):
+
+    def __init__(self, db_handler: DBHandler) -> None:
+        super().__init__(db_handler=db_handler, table_class=SizeDict)
