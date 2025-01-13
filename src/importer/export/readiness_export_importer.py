87761206dc6f8799127add95b91902de9913e541
Jangboo Lee <github@jangboolee.com>
2025-01-13 14:18:08 +0900

    Implement readiness data export importer

---
+++ src/importer/export/readiness_export_importer.py
@@ -0,0 +1,68 @@
+import pandas as pd
+
+from src.importer.base_xlsx_importer import BaseXlsxImporter
+from src.db.db_handler import DBHandler
+from src.db.db_schema import (
+    Readiness,
+    ActivityTypeDict,
+    RegionDict,
+    StatusDict,
+)
+
+
+class ReadinessExportImporter(BaseXlsxImporter):
+
+    def __init__(self, db_handler: DBHandler) -> None:
+        super().__init__(db_handler=db_handler, table_class=Readiness)
+        self.activitytype_id_mapper = None
+        self.region_id_mapper = None
+        self.status_id_mapper = None
+
+    def _get_all_mappers(self) -> bool:
+        """Overriden helper method to get all required mappers for importing
+        the Readiness export file
+
+        Returns:
+            bool: True if all getters are successful, False if not
+        """
+
+        return all(
+            [
+                self._get_id_mapper(ActivityTypeDict),
+                self._get_id_mapper(RegionDict, name_col="code"),
+                self._get_id_mapper(StatusDict),
+            ]
+        )
+
+    def _process_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Overridden helper method to process the Country data export file
+
+        Args:
+            df (pd.DataFrame): Raw dataframe of the Country data export file
+
+        Returns:
+            pd.DataFrame: Processed dataframe of Country data
+        """
+
+        # Drop Country column with multiple values per cell
+        df.drop("Country", axis=1, inplace=True)
+        # Get mapper to map region ID from region name
+        self._get_all_mappers()
+        # Map IDs using names
+        self._map_ids(
+            df, "Activity", "activity_type_id", self.activitytype_id_mapper
+        )
+        self._map_ids(df, "Region", "region_id", self.region_id_mapper)
+        self._map_ids(df, "Status", "status_id", self.status_id_mapper)
+        # Re-arrange column order
+        self._move_col(df, "activity_type_id", 1)
+        self._move_col(df, "region_id", 4)
+        self._move_col(df, "status_id", 8)
+        # Convert approve date to datetime object
+        df["Approved Date"] = pd.to_datetime(
+            df["Approved Date"], format="%b %d, %Y"
+        ).dt.date
+        # Rename columns
+        df.columns = self.cols
+
+        return df
