4a08b15dec5acf3431aba784e3d480198e260613
Jangboo Lee <github@jangboolee.com>
2025-01-14 14:01:20 +0900

    Add status node service

---
+++ src/kg/services/status_node_service.py
@@ -0,0 +1,12 @@
+from src.kg.services.base_node_service import NodeService
+from src.db.db_schema import StatusDict
+
+from neo4j import Session
+
+
+class StatusService(NodeService):
+
+    def __init__(self, session: Session) -> None:
+
+        super().__init__(session, StatusDict)
+        self.node_label = "Status"
