6592d1a641a2b826d08e68cf77c270f005647034
Jangboo Lee <github@jangboolee.com>
2025-01-14 13:39:20 +0900

    Add ESS category node service

---
+++ src/kg/services/nodes/ess_category_node_service.py
@@ -0,0 +1,12 @@
+from src.kg.services.nodes.base_node_service import NodeService
+from src.db.db_schema import EssCategoryDict
+
+from neo4j import Session
+
+
+class EssCategoryService(NodeService):
+
+    def __init__(self, session: Session) -> None:
+
+        super().__init__(session, EssCategoryDict)
+        self.node_label = "EssCategory"
